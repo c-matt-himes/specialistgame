a68fdadf64565b8d43ba382362e1c0a4f725772a
c_matt_himes <c.matt.himes@gmail.com>
2025-04-26 19:14:15 -0400

    add specific Specialists

--- specialist.py
+++ specialist.py
@@ -10,6 +10,46 @@ class Specialist():
         self.martial = ISV(1)
         self.leadership = ISV(1)
 
-Test_Spec = Specialist(1)
+    def get_name(self):
+        return self.name
 
-print(Test_Spec.diplo, Test_Spec.insurrection, Test_Spec.espionage, Test_Spec.martial, Test_Spec.leadership)
+    def TestCommand(self):
+        print(f"{self} has {self.diplo} diplomacy, {self.insurrection} insurrection, {self.espionage} espionage, {self.martial} martial, {self.leadership} leadership")
+
+class Diplomat(Specialist):
+    def __init__(self,rank):
+        super().__init__(rank)
+        self.diplo = ISV(20)
+
+class Guerilla(Specialist):
+    def __init__(self,rank):
+        super().__init__(rank)
+        self.insurrection = ISV(20)
+
+class Spy(Specialist):
+    def __init__(self,rank):
+        super().__init__(rank)
+        self.espionage = ISV(20)
+
+class Assassin(Specialist):
+    def __init__(self,rank):
+        super().__init__(rank)
+        self.martial = ISV(20)
+
+class Commander(Specialist):
+    def __init__(self,rank):
+        super().__init__(rank)
+        self.leadership = ISV(20)
+
+TestDiplo = Diplomat(1)
+TestGuerilla = Guerilla(1)
+TestSpy = Spy(1)
+TestAssassin = Assassin(1)
+TestCommander = Commander(1)
+
+
+TestDiplo.TestCommand()
+TestGuerilla.TestCommand()
+TestSpy.TestCommand()
+TestAssassin.TestCommand()
+TestCommander.TestCommand()
